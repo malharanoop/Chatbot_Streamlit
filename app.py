4502a4e95e7e5e310f615996f209cc3390b4e390
malharanoop <108321085+malharanoop@users.noreply.github.com>
2025-01-06 14:52:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("CAPL Chatbot")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+# Chat History
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("Say something")
+if prompt:
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=st.session_state.messages,
+            stream=True
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
